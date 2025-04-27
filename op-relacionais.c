2b94a606f466ec3a2b200b0ddac23888dcb47c36
Ribeiro <luciano.ribeirodev@gmail.com>
2025-04-27 13:46:13 +0000

    commit de operacao com operadores relacionais

---
+++ op-relacionais.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int valor1 = 15;
+    int valor2 = 10;
+
+    printf("Maior que: %d\n", valor1 > valor2);
+    printf("Menor que: %d\n", valor1 < valor2);
+    printf("Maior ou igual: %d\n", valor1 >= valor2);
+    printf("Menor ou igual: %d\n", valor1 <= valor2);
+    printf("Igual a: %d\n", valor1 == valor2);
+    printf("Diferete de: %d\n", valor1 != valor2);
+}
